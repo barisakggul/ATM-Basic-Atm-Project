bfe9fdc543f643c0095f27cb5820d46d89efe427
Baris Akgul <akgulbars35@gmail.com>
2023-12-10 22:08:19 +0300

    CONSTRUCTORS

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
 ﻿using System;
 
-public class CardHoler
+public class CardHolder
 {
+	String CardNum;
+	int pin;
+	String FirstName;
+	String LastName;
+	double balance;
+	public CardHolder(String CardNum, String FirstName, String LastName, double balance, int pin)
+	{
+		this.CardNum = CardNum;
+		this.FirstName = FirstName;
+		this.LastName = LastName;
+		this.balance = balance;
+		this.pin = pin;
 
+	}
 }
